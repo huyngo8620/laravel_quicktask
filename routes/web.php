5e8eb1a4d7951cfeb61d432983a4d91da3b21694
Ngo Huy <conghuy2k86@gmail.com>
2022-09-29 15:37:16 +0700

    chap7-middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)->middleware('admin');
 
 Route::prefix('posts')->name('posts')->controller(PostController::class)->group(function () {
     Route::get('', 'index')->name('index');
